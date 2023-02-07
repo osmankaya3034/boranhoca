2380d9b32704dbb3935f4d5d23bc718697d1047c
osmankaya3034 <herkan26@gmail.com>
2023-02-07 21:17:41 +0300

    todoreact

---
+++ src/Components/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+
+const Header = () => {
+  return (
+    <div>
+        <h1 className='text-danger bg-info'>
+            Osman ToDo List
+        </h1>
+    </div>
+  )
+}
+
+export default Header
